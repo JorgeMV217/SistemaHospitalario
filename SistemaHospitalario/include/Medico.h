6ee13d815fa56d8f1eff9d80e37fa96917763cde
Jorge Madrid <jmadridv217@duck.com>
2024-12-15 21:19:41 +0100

    Creación de Header Medico

--- SistemaHospitalario/include/Medico.h
+++ SistemaHospitalario/include/Medico.h
@@ -1,1 +1,20 @@
-﻿
+﻿#ifndef MEDICO_H
+#define MEDICO_H
+
+#include <string>
+#include <vector>
+
+class Medico {
+public:
+    std::string nombre;
+    int id;
+    std::string especialidad;
+
+    Medico(std::string nombre, int id, std::string especialidad);
+
+    static void altaMedico(std::vector<Medico>& medicos, std::string nombre, int id, std::string especialidad);
+    static void bajaMedico(std::vector<Medico>& medicos, int id);
+    static void listarMedicos(const std::vector<Medico>& medicos);
+};
+
+#endif
