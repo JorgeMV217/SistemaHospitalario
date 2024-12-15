923e5d359ec1072c051eee1870c7c5d6c05a91ef
Jorge Madrid <jmadridv217@duck.com>
2024-12-15 20:50:34 +0100

    Creación de header para Paciente

--- SistemaHospitalario/include/Paciente.h
+++ SistemaHospitalario/include/Paciente.h
@@ -1,1 +1,24 @@
-﻿
+﻿#ifndef PACIENTE_H
+#define PACIENTE_H
+
+#include <string>
+#include <vector>
+
+class Paciente {
+public:
+    std::string nombre;
+    int id;
+    std::string fechaIngreso;
+    std::string historialClinico;
+
+    Paciente(std::string nombre, int id, std::string fechaIngreso);
+    void modificarDatos(std::string nuevoNombre);
+    void registrarHistorial(std::string historial);
+
+    static void altaPaciente(std::vector<Paciente>& pacientes, std::string nombre, int id, std::string fechaIngreso);
+    static void bajaPaciente(std::vector<Paciente>& pacientes, int id);
+    static void listarPacientes(const std::vector<Paciente>& pacientes);
+};
+
+#endif
+
